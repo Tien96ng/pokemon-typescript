73926f1ea7e97af3e1b49e67e53402e9cc6da344
tien96ng <tien96ng@gmail.com>
2021-06-23 20:18:07 -0700

    Set increase number of pokemon queries

--- src/containers/PokemonsContainer.jsx
+++ src/containers/PokemonsContainer.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Pokemon } from '../components/Pokemon.jsx';
 import { GET_POKEMONS } from '../graphql/get-pokemons';
 
 export function PokemonsContainer() {
+  const [page, setPage] = useState(1);
+
 
   // Protecting against getting errors returned.
   // Will default Pokemons to an empty array (It's an arr according to the docs).
   // Data will default empty obj.
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-    variables: { first: 9 },
+    variables: { first: (page * 9) },
   });
 
   return (
@@ -24,6 +26,8 @@ export function PokemonsContainer() {
           />
         )
       }
+      <p>Page {page} </p>
+      <button type="click" onClick={() => setPage(page + 1)}>Next Page</button>
     </div>
   );
 }
